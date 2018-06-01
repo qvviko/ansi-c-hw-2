659d1bdc7f5a4bb49a99f8c12e386fa4032227a4
Vlad Savchuk <qvviko@gmail.com>
2018-06-01 15:55:59 +0300

    + stack

--- src/stack.c
+++ src/stack.c
@@ -3,6 +3,7 @@
 //
 
 #include "stack.h"
+#include "math.h"
 
 struct Node {
     double value;
@@ -10,6 +11,10 @@ struct Node {
     struct Node *prev;
 
 };
+
+struct Node *stack = NULL; // it is your stack to work with it
+
+int size();
 
 int push(double value) {
     // return the exit code:
@@ -19,19 +24,69 @@ int push(double value) {
 
     // the stack size is 50 elements
 
-    /* YOUR CODE */
+    //We need to check if the size of the stack is already 50
+    if (&stack == NULL) {
+        stack->value = value;
+        stack->prev = NULL;
+        stack->next = NULL;
+        return 0;
+    } else if (size() != 50) {
+        struct Node *new_node = (struct Node *) malloc(sizeof(struct Node));
+        new_node->value = value;
+        struct Node *old = stack;
+
+        stack = new_node;
+        stack->next = old;
+        old->prev = new_node;
+        return 0;
+    } else {
+        return 1;
+    }
+    return 2;
 }
 
 double pop() {
     // returns top value and delete it from stack
     // if stack is empty returns -infinity and print error message to the screen
     /* YOUR CODE */
+    if (&stack == NULL) {
+        printf("StackUnderflow: STACK IS EMPTY\n");
+        return -INFINITY;
+    } else {
+        double value_to_return = stack->value;
+        struct Node *old_node = stack;
+
+        stack = stack->next;
+        stack->prev = NULL;
+
+        free(old_node);
+        return value_to_return;
+    }
+
 }
 
 double pick() {
     // just returns the top value
     // if stack is empty returns -infinity and print error message to the screen
     /* YOUR CODE */
+    if (&stack == NULL) {
+        printf("StackUnderflow: STACK IS EMPTY\n");
+        return -INFINITY;
+    } else {
+        return stack->value;
+    }
 }
 
-struct Node *stack = NULL; // it is your stack to work with it
+int size() {
+    if (&stack == NULL) {
+        return 0;
+    } else {
+        struct Node *current = stack;
+        int i = 1;
+        while (current->next != NULL) {
+            ++i;
+            current = current->next;
+        }
+        return i;
+    }
+}
